66ce302ef37008a0fa1bca8f2113a072122d6487
Lucas <lucasmacielaguiar@gmail.com>
2022-05-18 23:18:30 -0300

    Setup extension first dependencies

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+function App() {
+  return <Button variant="contained"> Olá Mundo</Button>;
+}
+
+export default App;
