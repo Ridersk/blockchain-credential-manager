dcd4224274f519412a0445f8cf4a64b71c9bcc86
Lucas <lucasmacielaguiar@gmail.com>
2022-05-25 23:17:35 -0300

    Add input button

--- app/public/content_script/content.js
+++ app/public/content_script/content.js
@@ -15,8 +15,34 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     };
     sendResponse({});
   } else if (request.action == "installInputObserver") {
-    document.querySelector('input[type="password"]').onkeyup = (event) => {
+    document.querySelector('input[type="password"]')?.onkeyup = (event) => {
       console.log(event.target.value);
     };
   } else sendResponse({}); // Send nothing..
 });
+
+const interval = setInterval(() => {
+  clearInterval(interval);
+  // fetch(chrome.runtime.getURL("popup_credentials/index.html"))
+  //   .then((result) => result.text())
+  //   .then((html) => {
+  //     document.querySelector('input[type="password"]').insertAdjacentHTML("beforebegin", html);
+  //   });
+
+  const passwordInput = document.querySelector('input[type="password"]');
+
+  if (passwordInput) {
+    const size = passwordInput.offsetHeight * 0.8;
+  
+    const buttonContainer = document.createElement("div");
+    buttonContainer.id = "bcm-credentials-container";
+    buttonContainer.style.height = `${size}px`;
+    buttonContainer.style.width = `${size}px`;
+  
+    const buttonImg = document.createElement("img");
+    buttonImg.src = "https://cdn-icons-png.flaticon.com/512/2152/2152349.png";
+  
+    buttonContainer.appendChild(buttonImg);
+    passwordInput.parentNode.insertBefore(buttonContainer, passwordInput);
+  }
+}, 100);
