52dcf1c12e4ce7ebdec156236799cc173edfa5ee
Lucas <lucasmacielaguiar@gmail.com>
2022-06-18 13:22:35 -0300

    Add delete credential option in form

---
+++ app/src/components/credential/credential-warning-delete.tsx
@@ -0,0 +1,24 @@
+import WarningModal from "components/ui/modal/modal-warning";
+import { useTranslation } from "react-i18next";
+
+export type CredentialDeletionWarningProps = {
+  open: boolean;
+  onCancel: () => void;
+  onAccept: () => void;
+};
+
+const CredentialDeletionWarningModal = ({ open, onCancel, onAccept }: CredentialDeletionWarningProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <WarningModal
+      open={open}
+      title={t("warning_delete_credential_title")}
+      description={t("warning_delete_credential_description")}
+      onCancel={onCancel}
+      onAccept={onAccept}
+    />
+  );
+};
+
+export default CredentialDeletionWarningModal;
