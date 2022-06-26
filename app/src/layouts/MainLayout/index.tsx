d0806fd7b659192744ed5eeb8a9937b0f73bb3b8
Lucas <lucasmacielaguiar@gmail.com>
2022-06-26 10:47:26 -0300

    Fix no scroll bug

--- app/src/layouts/MainLayout/index.tsx
+++ app/src/layouts/MainLayout/index.tsx
@@ -5,9 +5,26 @@ import Header from "./Header";
 
 const MainLayout = () => {
   return (
-    <Box sx={{ display: "block", position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}>
-      <Header sx={{ marginBottom: "32px" }} />
-      <Container sx={{ width: { xs: "400px", sm: "100%" }, height: { xs: "600px", sm: "100%" } }}>
+    <Box
+      sx={{
+        display: "block",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        overflow: "auto"
+      }}
+    >
+      <Header
+        sx={{
+          marginBottom: "32px"
+        }}
+      />
+      <Container
+        sx={{
+          width: { xs: "400px", sm: "100%" },
+          height: { xs: "600px", sm: "100%" }
+        }}
+      >
         <Outlet />
       </Container>
     </Box>
