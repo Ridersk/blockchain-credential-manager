2d3c5fe57bf37714ef86691b7674dd4b93ec1dce
Lucas Maciel Aguiar <lucasmacielaguiar@gmail.com>
2022-10-03 23:35:04 -0300

    Disable alarms

--- app/src/scripts/background.ts
+++ app/src/scripts/background.ts
@@ -1,11 +1,9 @@
-import { setupAlarms } from "./workers/alarms";
 import { setupPersistentWorker } from "./workers/persistent-worker";
 import { setupVault } from "./workers/vault";
 
 setupApp();
 
 export async function setupApp() {
-  setupAlarms();
   setupPersistentWorker();
   await setupVault();
 }
